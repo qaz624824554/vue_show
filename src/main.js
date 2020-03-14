c72c965d3afab6a99cda9a593a335d9996831f23
czl <1093978692@qq.com>
2020-03-14 15:35:44 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+
+// 设置url
+axios.defaults.baseURL = 'http://127.0.0.1:8081/api/private/v1/'
+// 挂载
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
